3ac9f21c1d4147805bbea7e241364f9a5def951e
Sapsan <semya_s@mail.ru>
2022-12-24 23:40:09 +0300

    Задача 41 в2: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. //0, 7, 8, -2, -2-> 2 //1, -7, 567, 89, 223-> 4

--- Task41/Task41/Task41/Program.cs
+++ Task41/Task41/Task41/Program.cs
@@ -48,6 +48,45 @@ int summ()
         }
 
         return s;
+}
+
+//Решение преподователя
+//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+
+using System;
+using System.Net.NetworkInformation;
+using static System.Net.Mime.MediaTypeNames;
+
+Console.WriteLine("Введите последовательность чисел через пробел и узнайте сколько чисел больше нуля Вы ввели");
+
+int PositiveNumbersInString()
+{
+    string text = Console.ReadLine();
+    int[] array = GetNumbers(text);
+    return GetCount(array);
+}
+
+Console.WriteLine(PositiveNumbersInString());
+
+int[] GetNumbers(string input)
+{
+    int[] array = input.Split(' ').
+    Where(x => !string.IsNullOrWhiteSpace(x)).
+    Select(x => int.Parse(x)).ToArray();
+    return array;
+}
+
+
+int GetCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            count++;
     }
-
-
+    return count;
+}
